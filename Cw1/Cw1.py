fc525b2d9b38692731256f74ed664c3cd1e97dde
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-02-28 15:11:28 +0100

    Create Cw1.py

---
+++ Cw1/Cw1.py
@@ -0,0 +1,14 @@
+import typing
+
+class matrix:
+    def __init__(self, matrix: List(List)):
+        self.__matrix_ = matrix
+
+    def __add__():
+        pass
+
+    def __mul__():
+        pass
+
+    def __str__():
+        pass
