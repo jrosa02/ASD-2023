4952c13454ee1c18b997d178986594937aa9cbd0
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-02-28 16:11:14 +0100

    Cw1 done

--- Cw1/Cw1.py
+++ Cw1/Cw1.py
@@ -1,17 +1,58 @@
-import typing
+from typing import List
 
-class matrix:
-    def __init__(self, matrix: List(List)):
+class macierz:
+    def __init__(self, matrix):
         self.__matrix_ = matrix
 
-    def __add__():
-        pass
+    def __getitem__(self, cord):
+        return self.__matrix_[cord]
 
-    def __mul__():
-        pass
+    def __add__(self, other):
+        matrix = []
+        for rownr in range(len(self.__matrix_)):
+            row_ = []
+            for col in range(len(self.__matrix_[0])):
+                row_.append(self[rownr][col] + other[rownr][col])
+            matrix.append(row_)
+        return macierz(matrix)
 
-    def __str__():
-        pass
+    def __mul__(self, other):
+        new_matrix = []
+        for new_row_nr_i in range(self.size()[0]):
+            new_row = []
+            for new_col_nr_i in range(self.size()[0]):
+                new_val = 0
+                for i in range(self.size()[1]):
+                    new_val += self[new_col_nr_i][i] * other[i][new_row_nr_i]
+                new_row.append(new_val)
+            new_matrix.append(new_row)
+        return macierz(new_matrix)
+        
+    def __str__(self):
+        row_q, col_q = self.size()
+        strink = ""
+        for row_nr in range(row_q):
+            strink += "| "
+            for col_nr in range(col_q):
+                strink += str(self[row_nr][col_nr]) + " "
+            strink += "|\n"
+        return strink
 
-    def __getitem__():
-        pass
+    def size(self):
+        return (len(self.__matrix_), len(self.__matrix_[0]))
+
+
+m1 = macierz(
+[ [1, 0, 2],
+  [-1, 3, 1] ]
+)
+
+m2 = macierz(
+[ [3, 1],
+  [2, 1],
+  [1, 0]]
+)
+
+print(m1.size())
+print(m1[0][0])
+print(m1 * m2)
