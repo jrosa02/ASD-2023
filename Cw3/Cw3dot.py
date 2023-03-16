631e0be5ee34088a90a572dd73f005049f25d693
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-03-16 17:41:13 +0100

    Update Cw3dot.py

--- Cw3/Cw3dot.py
+++ Cw3/Cw3dot.py
@@ -1,11 +1,12 @@
 size = 0
 
 class element:
-    def __init__(self, next = None, data: list = None) -> None:
-        self.tab_: list= []
+    def __init__(self, next = None, datalist: list = None) -> None:
+        self.tab_: list= [None for _ in range(size)]
+        if datalist is not None:
+            for i in range(len(datalist)):
+                self.tab_[i] = datalist[i]
         self.next_: element = next
-        if not data is None:
-            self.tab_ = data
             
     def get(self, index: int):
         elem = self
@@ -21,21 +22,59 @@ class element:
         połowa zapełnionej tablicy jest przenoszona do nowego elementu i wstawienie danej 
         zachodzi albo w opróżnianym elemencie albo we wstawianym (w zależności gdzie 'wypada' miejsce wskazane przez indeks). 
         Podanie indeksu większego od aktualnej liczby elementów listy skutkuje dodaniem elementu na końcu listy."""
-        if len(self.tab_) < size and index < len(self.tab_):
-            self.tab_ = self.tab_[:index] + [data] + self.tab_[index:]
-        if len(self.tab_) < size and index >= len(self.tab_):
-            self.tab_.append(data)
+        if index < size:
+            if self.tab_[index] is None:
+                self.tab_[index] = data
+                #print("zastap None")
+            else:
+                self.tab_ = self.tab_[:index] + [data] + self.tab_[index:]
+                #print("wstaw i rozson")
+                if self.tab_[-1] is None:
+                    #print("usun None")
+                    self.tab_.pop()
+        elif self.tab_[-1] is None and self.next_ is None:
+                #print("append")
+                i =0 
+                if self.tab_[0] is not None:
+                    while self.tab_[i] is not None:
+                        i+=1    
+                self.tab_[i] = data
+        if index >= size and self.tab_[-1] is not None:
+            if self.next_ is None:
+                #print("nowy element")
+                lst = [data]
+                self.next_ = element(next= None, datalist=[data])
+            elif self.next_ is not None:
+                self.next_.insert(data, index-len(self.tab_))
 
-        if
-        self.next_ = element(next= None, data= self.tab_[size//2:])
-        self.tab_ = self.tab_[:size//2]
+
+        #Przed dodaniem rozsuwania wszystko było o wiele bardziej
+        # if len(self.tab_) > size:
+        #     elems: element = element(next=self.next_, datalist= self.tab_[size//2:])
+        #     tabl: list= [None for _ in range(size)]
+        #     if self.tab_[:size//2] is not None:
+        #         for i in range(len(self.tab_[:size//2])):
+        #             tabl[i] = self.tab_[:size//2][i]
+        #     self.tab_ = tabl
+        #     self.next_ = elems
+
             
 
     def delete(self, index: int):
         pass
 
+    def __str__(self):
+        out = "-----------------------------------\n"
+        out += str(self.tab_) + "\n"
+        elem = self
+        while elem.next_ is not None:
+            elem = elem.next_
+            out += "->" + str(elem.tab_) + "\n"
+        out += ("-----------------------------------")
+        return out
+
     def debug(self):
-        print(self.tab_)
+        print(self)
 
 
 
@@ -43,14 +82,16 @@ if __name__ == "__main__":
     size = 6
     elem = element()
     elem.debug()
-    print()
+    print("Inserting")
     for i in range(0,10):
         elem.insert(data = i, index = i)
         elem.debug()
-    print(elem.get(4))
+    print("Getting" + str(elem.get(4)))
     elem.insert(10,1)
     elem.insert(11,8)
+    print("Random insert")
     elem.debug()
     elem.delete(1)
     elem.delete(2)
+    print("Deleting")
     elem.debug()
