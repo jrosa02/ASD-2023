eceabe8f27819da6543e2f9642d550be2391ac3c
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-03-17 17:45:13 +0100

    new insert is working

--- Cw3/Cw3dot.py
+++ Cw3/Cw3dot.py
@@ -22,8 +22,70 @@ class element:
         if elem is not None:
             return elem.tab_[index]
         
+    def append_forNone(self, data):
+        print("append")
+        i =0 
+        if self.tab_[0] is not None:
+            while self.tab_[i] is not None:
+                i+=1
+        self.tab_[i] = data
+
+    def insert2tab(self, data, index):
+        print("index < cap")
+        if self.tab_[index] is None:
+            self.tab_[index] = data
+            print("zastap None")
+        else:
+            self.tab_ = self.tab_[:index] + [data] + self.tab_[index:]
+            print("wstaw i rozson")
+            if self.tab_[-1] is None:
+                print("usun None")
+                self.tab_.pop()
+
+    def delete_overflow(self):
+        #Przed dodaniem rozsuwania wszystko było o wiele bardziej
+        if len(self.tab_) > capacity:
+            self.divide()
+
+
+    def divide(self):
+        elems: element = element(next=self.next_, datalist= self.tab_[capacity//2:])
+        tabl: list= [None for _ in range(capacity)]
+        if self.tab_[:capacity//2] is not None:
+            for i in range(len(self.tab_[:capacity//2])):
+                tabl[i] = self.tab_[:capacity//2][i]
+        self.tab_ = tabl
+        self.next_ = elems
+        
 
     def insert(self, data, index: int):
+        """wstawiająca daną w miejscu wskazanym przez podany indeks, przesuwając istniejące elementy w prawo;
+        jeżeli tablica elementu w którym ma nastąpić wstawienie jest pełna to do listy dokładany jest nowy element, 
+        połowa zapełnionej tablicy jest przenoszona do nowego elementu i wstawienie danej 
+        zachodzi albo w opróżnianym elemencie albo we wstawianym (w zależności gdzie 'wypada' miejsce wskazane przez indeks). 
+        Podanie indeksu większego od aktualnej liczby elementów listy skutkuje dodaniem elementu na końcu listy."""
+        print("Inserting")
+        if index < len(self):
+            print("index < capacity and index < len(self)")
+            self.insert2tab(data, index)
+        elif self.tab_[-1] is None and self.next_ is None and index < capacity:
+            print("Append")
+            self.append_forNone(data)
+        elif index > len(self):
+            if self.next_ is None:
+                self.tab_.append(data)
+            else:
+                self.next_.insert(data, index-len(self))
+
+
+        
+
+
+        
+            
+        self.delete_overflow()
+
+    def insert_old(self, data, index: int):
         """wstawiająca daną w miejscu wskazanym przez podany indeks, przesuwając istniejące elementy w prawo;
         jeżeli tablica elementu w którym ma nastąpić wstawienie jest pełna to do listy dokładany jest nowy element, 
         połowa zapełnionej tablicy jest przenoszona do nowego elementu i wstawienie danej 
@@ -58,16 +120,7 @@ class element:
             elif self.next_ is not None:
                 self.next_.insert(data, index-len(self.tab_))
 
-
-        #Przed dodaniem rozsuwania wszystko było o wiele bardziej
-        if len(self.tab_) > capacity:
-            elems: element = element(next=self.next_, datalist= self.tab_[capacity//2:])
-            tabl: list= [None for _ in range(capacity)]
-            if self.tab_[:capacity//2] is not None:
-                for i in range(len(self.tab_[:capacity//2])):
-                    tabl[i] = self.tab_[:capacity//2][i]
-            self.tab_ = tabl
-            self.next_ = elems
+        self.delete_overflow()
 
     def delete(self, index: int):
         pass
@@ -130,9 +183,10 @@ if __name__ == "__main__":
         elem.insert(data = i, index = i)
         elem.debug()
     print("Getting [4]: " + str(elem.get(4)))
+    print("Random insert (10,1),(11,8)")
     elem.insert(10,1)
     elem.insert(11,8)
-    print("Random insert")
+
     elem.debug()
     elem.delete(1)
     elem.delete(2)
