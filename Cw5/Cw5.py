167a8b495ad8b48ddf1c965b75f3e2cab6ea5175
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-03-28 16:08:23 +0200

    Update Cw5.py

--- Cw5/Cw5.py
+++ Cw5/Cw5.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Node:
     def __init__(self, key, value = None, left_desc = None, right_desc = None) -> None:
         self.key_ = key
@@ -5,13 +8,16 @@ class Node:
         self.left_desc_: Node = left_desc
         self.right_desc_: Node = right_desc
 
-    def search(self, key):
+    def __str__(self) -> str:
+        return str(self.key_) + "-" + str(self.value_)
+
+    def search(self, key, prev):
         if self.key_ == key:
-            return self
+            return self, prev
         elif self.key_ < key and self.left_desc_ is not None:
-            self.left_desc_.search(key)
+            return(self.left_desc_.search(key, self))
         elif self.key_ > key and self.right_desc_ is not None:
-            self.right_desc_.search(key)
+            return(self.right_desc_.search(key, self))
         else:
             return None
         
@@ -30,8 +36,26 @@ class Node:
             else:
                 self.right_desc_ = Node(key, value)
 
-    def delete(self):
-        pass
+    def delete(self, prev):
+        prev: Node = prev
+        if self.left_desc_ is None and self.right_desc_ is None:
+            if prev.key_ < self.key_:
+                prev.left_desc_ = None
+            else:
+                prev.right_desc_ = None
+        elif self.left_desc_ is not None:
+            if prev.key_ < self.key_:
+                prev.left_desc_ = self.left_desc_
+            else:
+                prev.right_desc_ = self.left_desc_
+        elif self.right_desc_ is not None:
+            if prev.key_ < self.key_:
+                prev.left_desc_ = self.right_desc_
+            else:
+                prev.right_desc_ = self.right_desc_
+        else:
+            pass
+
 
     def height(self) -> int:
         pass
@@ -42,8 +66,10 @@ class BST:
 
     def search(self, key):
         if self.root_ is not None:
-            self.root_.search(key).value_
-
+            return self.root_.search(key, self.root_)[0].value_
+        else: 
+            return None
+        
     def insert(self, key, value):
         if self.root_ is not None:
             self.root_.insert(key, value)
@@ -51,7 +77,11 @@ class BST:
             self.root_ = Node(key, value)
 
     def delete(self, key):
-        self.root_.search(key).delete()
+        node, prev = self.root_.search(key, self.root_)
+        node.delete(prev)
+
+    def height(self):
+        pass
 
     def print_tree(self):
         print("==============")
@@ -73,4 +103,7 @@ if __name__ == "__main__":
     tree = BST()
     for key in keyvalue:
         tree.insert(key, keyvalue[key])
+    tree.print_tree()
+    print(tree.search(60))
+    tree.delete(37)
     tree.print_tree()
