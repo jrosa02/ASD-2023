e3ee55a5fd7cc8659efd2bc68bdb9d5ee596da62
Jan Rosa <jrosa@student.agh.edu.pl>
2023-04-22 15:55:41 +0200

    done

--- Cw7/Cw7lst.py
+++ Cw7/Cw7lst.py
@@ -18,10 +18,10 @@ class Vertex():
         return str(self.key) + " -> " + str(self.data)
 
 
-class matGraf():
+class lstGraf():
     def __init__(self):
         """konstruktor - tworzący pusty graf"""
-        self.prox_matrix:np.ndarray = np.ndarray((0,0))
+        self.prox_list = []
         self.index_dict = dict()
 
     # isEmpty( ) - zwracająca True jeżeli graf jest pusty
@@ -32,35 +32,40 @@ class matGraf():
     
     def insertVertex(self, vertex):
         """insertVertex(vertex)    - wstawia do grafu  podany węzeł"""
-        if self.prox_matrix.shape == (0, 0):
-            self.prox_matrix = np.zeros((1, 1))
-            self.index_dict[vertex] = 0
-        else:
-            self.index_dict[vertex] = self.prox_matrix.shape[0]
-            self.prox_matrix = np.concatenate([self.prox_matrix, np.zeros((1, self.prox_matrix.shape[1]))])
-            self.prox_matrix = np.concatenate([self.prox_matrix, np.zeros((self.prox_matrix.shape[0], 1))], axis=1)
+        self.index_dict[vertex] = len(self.prox_list)
+        self.prox_list.append([])
+        #edges = self.edges()
     
     def insertEdge(self, vertex1, vertex2, edge = 1):
         """insertEdge(vertex1, vertex2, egde) - wstawia do grafu krawędź pomiędzy podane węzły"""
-        self.prox_matrix[self.index_dict[vertex1], self.index_dict[vertex2]] = edge
+        self.prox_list[self.index_dict[vertex1]].append(self.index_dict[vertex2])
 
     # deleteVertex(vertex) - usuwa podany węzeł
     def deleteVertex(self, vertex):
         index = self.index_dict.pop(vertex)
-        self.prox_matrix = np.delete(self.prox_matrix, index, 0)
-        self.prox_matrix = np.delete(self.prox_matrix, index, 1)
+        self.prox_list.pop(index)
         for vtx in self.index_dict:
             if self.index_dict[vtx] > index:
                 self.index_dict[vtx] -= 1
+        for i in range(len(self.prox_list)):
+            self.prox_list[i] = list(filter(lambda a: a!= index, self.prox_list[i]))
+            for j in range(len(self.prox_list[i])):
+                self.prox_list[i][j] -= 1 if self.prox_list[i][j] > index else 0
+
     
     # deleteEdge(vertex1, vertex2) - usuwa krawędź pomiędzy podanymi węzłami
     def deleteEdge(self, vertex1, vertex2):
-        self.insertEdge(vertex1, vertex2, 0)
+        idx1 = self.getVertexIdx(vertex1)
+        idx2 = self.getVertexIdx(vertex2)
+        
+        if self.prox_list[idx1] is not None:
+            idx2 = self.prox_list[idx1].index(idx2)
+            self.prox_list[idx1].pop(idx2)
 
     # Dodatkowo przydatne będą metody metody:
     def getVertexIdx(self, vertex) -> int:
         """getVertexIdx(vertex) - zwraca indeks węzła (wykorzystując metodę indeks lub wspomniany słownik)"""
-        raise NotImplementedError()
+        return self.index_dict[vertex]
     
     def getVertex(self, vertex_idx) -> Vertex:
         """getVertex(vertex_idx)    - zwraca węzeł o podanym indeksie (niejako odwrotność powyższej metody)"""
@@ -83,36 +88,29 @@ class matGraf():
     
     def size(self, directed: bool = False):
         """size() - zwraca rozmiar grafu (liczbę krawędzi)"""
-        size = 0
-        for row in range(self.prox_matrix.shape[0]):
-            for col in range(self.prox_matrix.shape[0]):
-                if row != col and self.prox_matrix[row, col] != 0:
-                    size += 1
-        if directed:
-            size /= 2
-        return size
+        raise NotImplementedError()
      
     def edges(self, directed: bool = False):
         """edges() - zwracająca wszystkie krawędzie grafu w postaci listy par: (klucz_węzła_początkowego, klucz_węzła_końcowego) 
         - będzie potrzebna do wyrysowania grafu."""
         lst = list()
         if not directed:
-            for row in range(self.prox_matrix.shape[0]):
-                for col in range(row):
-                    if row != col and self.prox_matrix[row, col] != 0:
-                        lst.append((self.getVertex(row).key, self.getVertex(col).key))
+            for i in range(len(self.prox_list)):
+                for somsiad_idx in self.prox_list[i]:
+                        lst.append((self.getVertex(i).key, self.getVertex(somsiad_idx).key))
         return lst
     
 
 if __name__ == "__main__":
     print(Vertex('K', pl.slownik['K']) == Vertex('Z', pl.slownik['Z']))
-    matrixgraf = matGraf()
+    listgraf = lstGraf()
     for woj in pl.slownik:
         x = Vertex(woj, pl.slownik[woj])
-        matrixgraf.insertVertex(x)
+        listgraf.insertVertex(x)
     for woj in pl.graf:
-        matrixgraf.insertEdge(Vertex(woj[0], pl.slownik[woj[0]]), Vertex(woj[1], pl.slownik[woj[1]]))
-    matrixgraf.deleteVertex(Vertex('K', pl.slownik['K']))
-    matrixgraf.deleteEdge(Vertex('W', pl.slownik['W']), Vertex('E', pl.slownik['E']))
-    edges = matrixgraf.edges()
+        listgraf.insertEdge(Vertex(woj[0], pl.slownik[woj[0]]), Vertex(woj[1], pl.slownik[woj[1]]))
+    listgraf.deleteVertex(Vertex('K', pl.slownik['K']))
+    listgraf.deleteEdge(Vertex('W', pl.slownik['W']), Vertex('E', pl.slownik['E']))
+    #listgraf.deleteEdge(Vertex('E', pl.slownik['E']), Vertex('W', pl.slownik['W']))
+    edges = listgraf.edges()
     pl.draw_map(edges)
